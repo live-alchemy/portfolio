85477fe73b8737180a3b07bbe5d59aa7972f3a87
live-alchemy <live.alchemy@protonmail.com>
2023-06-13 13:07:49 -0700

    payment button links for mens event

--- src/pages/mens-event.js
+++ src/pages/mens-event.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Widget } from "@typeform/embed-react";
 import mtns from "@images/mtns.jpg";
 import Header from "@components/layout/Header";
 
@@ -24,18 +23,11 @@ function Event() {
                 className="column has-text-centered-touch is-12-mobile is-10-tablet is-offset-1-tablet is-5-desktop is-offset-1-desktop is-5-widescreen is-offset-1-widescreen is-5-fullhd is-offset-1-fullhd aos-init aos-animate"
               >
                 <h1 className="title is-1">Men's Work Weekend</h1>
-                <h4 className="title is-4">Interested in joining?</h4>
-                <h4 className="title is-4">
-                  We are currently croudsourcing to determine dates, location
-                  and price!
-                </h4>
-                <h4 className="title is-4">
-                  Fill out a registration form to have your voice heard.
-                </h4>
-                {/* <h3 className="title is-3">October 27-29th, 2023</h3>
-
-                <h4 className="title is-4">$250, potluck style meals</h4>
-                <p className="is-size-5">On the shores of Anderson Lake, BC</p> */}
+                <h3 className="title is-3">October 27-29th, 2023</h3>
+                <p className="is-size-5">
+                  An immersive weekend experience for men doing the work. On the
+                  shores of Anderson Lake, BC
+                </p>{" "}
                 <p className="mt-5">
                   <a href="#schedule" className="button is-success mr-2">
                     Schedule
@@ -52,8 +44,7 @@ function Event() {
       <section className="hero is-medium" id="schedule">
         <div className="hero-body">
           <div className="container">
-            <h2 className="title is-2">Weekend Schedule</h2>
-            <p>Proposed activites for the weekend</p>
+            <h2 className="title is-2">Schedule</h2>
             <hr />
             <div className="columns">
               <div className="column">
@@ -91,13 +82,13 @@ function Event() {
                   <b>12pm</b> Paddling & hiking adventure
                 </p>
                 <p>
-                  <b>4pm</b> Down time and prep
+                  <b>4pm</b> Down time and meal prep
                 </p>
                 <p>
-                  <b>5pm</b> Talent show & skill share
+                  <b>6pm</b> Dinner and Gratitude Circle
                 </p>
                 <p>
-                  <b>7pm</b> Dinner and circle
+                  <b>730pm</b> Talent show & skill share
                 </p>
               </div>
               <div className="column">
@@ -115,17 +106,24 @@ function Event() {
                   <b>10am</b> Shadow work
                 </p>
                 <p>
-                  <b>12pm</b> Solo time
+                  <b>12pm</b> Solo Integration time
                 </p>
                 <p>
                   <b>2pm</b> Circle
                 </p>
                 <p>
-                  <b>5pm</b> Sauna
+                  <b>5pm</b> Sauna and Sound
                 </p>
                 <p>
-                  <b>7pm</b> Dinner
+                  <b>7pm</b> Dinner & Closing Circle
                 </p>
+              </div>
+            </div>
+            <div className="columns">
+              <div className="column is-12 has-text-centered py-6">
+                <a href="#register" className="button is-danger">
+                  Register
+                </a>
               </div>
             </div>
           </div>
@@ -135,10 +133,73 @@ function Event() {
         <div className="hero-body">
           <div className="container">
             <h2 className="title is-2" id="register">
-              Register
+              Registration
             </h2>
             <hr />
-            <Widget id="s3VemTmm" style={{ width: "100%", height: "95vh" }} />
+            <p className="title is-4">
+              Secure your spot. This container will hold 8 men in total.
+            </p>
+            <p className="title is-6">
+              Price covers venue rental, sauna use, accomodations and food
+              basics.
+            </p>
+            <p className="mb-4">
+              Choose the pricing option that works for you. If money is not
+              available, there are two seats that can be made available for
+              those who want to assist in other ways, reach out:{" "}
+              <a href="mailto:live.alchemy@proton.me">live.alchemy@proton.me</a>
+            </p>
+            <div className="columns py-4">
+              <div className="column">
+                <div className="box has-text-centered py-5">
+                  <h4 className="title is-3">Ballin on a budget</h4>
+                  <a
+                    href="https://buy.stripe.com/dR6aHhfO66bU34seUU"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="button is-secondary"
+                  >
+                    Register for $350
+                  </a>
+                </div>
+              </div>
+              <div className="column">
+                <div className="box has-text-centered py-5">
+                  <h4 className="title is-3">The middle path</h4>
+                  <a
+                    href="https://buy.stripe.com/6oEg1BeK21VE48wdQR"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="button is-primary"
+                  >
+                    Register for $425
+                  </a>
+                </div>
+              </div>
+              <div className="column">
+                <div className="box has-text-centered py-5">
+                  <h4 className="title is-3">Flex</h4>
+                  <a
+                    href="https://buy.stripe.com/dR63ePdFY6bUfRe28a"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="button is-warning"
+                  >
+                    Register for $500
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="columns py-4">
+              <div className="column is-12">
+                <p className="has-text-centered">
+                  Need to pay by another method? Let me know:{" "}
+                  <a href="mailto:live.alchemy@proton.me">
+                    live.alchemy@proton.me
+                  </a>
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </section>
