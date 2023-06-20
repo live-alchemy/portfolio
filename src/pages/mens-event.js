523643e34bfae49914ea0d9c234e89718207843c
live-alchemy <live.alchemy@protonmail.com>
2023-06-20 15:03:43 -0700

    why mens work

--- src/pages/mens-event.js
+++ src/pages/mens-event.js
@@ -327,6 +327,57 @@ function Event() {
           </div>
         </div>
       </section>
+      <section className="hero is-dark">
+        <div className="hero-body">
+          <div className="container">
+            <h2 className="title is-2" id="register">
+              Why men's work
+            </h2>
+            <hr />
+            <h4 className="title is-4">
+              Men’s work is about empowering positive Masculinity and supporting
+              men through brotherhood.
+            </h4>
+            <div className="columns py-2">
+              <div className="column">
+                <h5 className="title is-5">Warrior: Activation practices</h5>
+                <p>
+                  Cultivate masculine energy by working hard, exploring, moving
+                  and expressing your authentic nature in the company and
+                  presence of other conscious men.
+                </p>
+                <h5 className="title is-5 mt-4">
+                  Sage: Connect to your deeper feelings
+                </h5>
+                <p>
+                  Knowing yourself allows you to connect deeper with others and
+                  provide leadership informed by empathy and a strong and
+                  reliable intuitive understanding.
+                </p>
+              </div>
+              <div className="column">
+                <h5 className="title is-5">Alex's Approach</h5>
+                <p>
+                  Combining a decade of experience with breathwork, martial
+                  practice and Qi Gong, Alex works with these disciplines to
+                  activate yang energy. As a former musician and performer, he
+                  also creates a safe space for men to connect with their
+                  expressive natures and be seen in a shame free environment.
+                </p>
+                <p>
+                  Having experienced tremendous healing through shadow work,
+                  circle, counseling, therapy and, of course, men's work, Alex
+                  will facilitate circles and shadow work for the group informed
+                  by his own experience in unlocking shame, fear and toxic
+                  masculine energy (aka nice guy syndrome) to bring about a felt
+                  transformation and deepening in trustability for each man
+                  ready to take the plunge.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
       <section className="hero is-success">
         <div className="hero-body">
           <div className="container">
