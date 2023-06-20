dedb2bb0a7099e93dfd69b6547be9bdce266ad13
live-alchemy <live.alchemy@protonmail.com>
2023-06-20 13:40:37 -0700

    add location info

--- src/pages/mens-event.js
+++ src/pages/mens-event.js
@@ -3,6 +3,7 @@ import { BsSignal } from "react-icons/bs";
 import { Link } from "gatsby";
 import mtns from "@images/mtns.jpg";
 import alex from "@images/alex.jpg";
+import andersonlake from "@images/andersonlake.jpg";
 import Header from "@components/layout/Header";
 
 function Event() {
@@ -111,16 +112,13 @@ function Event() {
                   <b>10am</b> Shadow work
                 </p>
                 <p>
-                  <b>12pm</b> Solo Integration time
+                  <b>12pm</b> Integration time
                 </p>
                 <p>
-                  <b>2pm</b> Circle
+                  <b>2pm</b> Sauna and Sound
                 </p>
                 <p>
-                  <b>5pm</b> Sauna and Sound
-                </p>
-                <p>
-                  <b>7pm</b> Dinner & Closing Circle
+                  <b>4pm</b> Circle & Dinner
                 </p>
               </div>
             </div>
@@ -145,8 +143,7 @@ function Event() {
               Secure your spot. This container will hold 8 men in total.
             </p>
             <p className="title has-text-black is-6">
-              Price covers venue rental, sauna use, accomodations and food
-              basics.
+              Price covers venue rental, sauna use, accomodations and food.
             </p>
             <p className="mb-4">
               Choose the pricing option that works for you. If money is not
@@ -272,6 +269,56 @@ function Event() {
                       <span className="icon">
                         <BsSignal />
                       </span>
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="hero is-link is-medium">
+        <div className="hero-body">
+          <div className="container">
+            <h2 className="title is-2" id="register">
+              Location
+            </h2>
+            <hr />
+            <div class="container">
+              <div class="columns is-multiline is-vcentered">
+                <div
+                  data-aos="fade-left"
+                  class="column is-10-mobile is-offset-1-mobile is-10-tablet is-offset-1-tablet is-4-desktop is-offset-1-desktop is-4-widescreen is-offset-1-widescreen is-4-fullhd is-offset-1-fullhd aos-init aos-animate"
+                >
+                  <figure class="image feather">
+                    <img
+                      style={{ borderRadius: "5px" }}
+                      alt="Anderson Lake"
+                      src={andersonlake}
+                    />
+                  </figure>
+                </div>
+
+                <div
+                  data-aos="fade-down"
+                  class="column has-text-centered-touch is-12-mobile is-10-tablet is-offset-1-tablet is-5-desktop is-offset-1-desktop is-5-widescreen is-offset-1-widescreen is-5-fullhd is-offset-1-fullhd aos-init aos-animate"
+                >
+                  <h1 class="title is-3">Anderson Lake, BC</h1>
+                  <p class="is-size-4">A rugged and wild setting</p>
+                  <p className="is-size-5 mt-1">
+                    Location specifics will be sent after registration and car
+                    pooling will be arranged. Budget roughly 4 hours from
+                    Vancouver and expect a rugged gravel road and a steep
+                    driveway for the final 25km.
+                  </p>
+                  <p className="is-size-6 mt-5">
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href="https://www.google.com/maps/dir/Vancouver,+BC/Seton+Portage,+BC+V0N+3B0/@49.9893421,-124.1008358,8z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x548673f143a94fb3:0xbb9196ea9b81f38b!2m2!1d-123.1207375!2d49.2827291!1m5!1m1!1s0x54809a9006608a15:0xaa059afe87711ba8!2m2!1d-122.2895451!2d50.707377?entry=ttu"
+                      className="button is-info"
+                    >
+                      <span>Open in Maps</span>
                     </a>
                   </p>
                 </div>
