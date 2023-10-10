5775fa02cef9a5c4a238d2b6406acbdde333e458
live-alchemy <live.alchemy@protonmail.com>
2023-10-09 23:06:30 -0700

    install netlify build package

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,7 @@ module.exports = {
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-netlify",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
